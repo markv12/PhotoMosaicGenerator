94a1f244017a5359d9778ed0b737cbb74f1e2c8e
Scott Steffes <swsteffes@gmail.com>
2021-03-06 20:42:30 +0900

    Improve performance

--- Assets/Scripts/MosaicManager.cs
+++ Assets/Scripts/MosaicManager.cs
@@ -35,7 +35,19 @@ public class MosaicManager : MonoBehaviour
         for (int i = 0; i < chunksToPullFrom.Length; i++)
         {
             TextureChunkData candidateChunk = chunksToPullFrom[i];
-            int difference = mainChunk.DiffWithChunk(candidateChunk);
+            int difference = 0;
+            for (int j = 0; j < mainChunk.colors.Length; j++)
+            {
+                Color32 color1 = mainChunk.colors[j];
+                Color32 color2 = candidateChunk.colors[j];
+                int bw1 = color1.r * 30 + color1.g * 59 + color1.b * 11;
+                int bw2 = color2.r * 30 + color2.g * 59 + color2.b * 11;
+                int bwDiff = bw1 - bw2;
+
+                bwDiff = (bwDiff + (bwDiff >> 31)) ^ (bwDiff >> 31); //Absolute Value
+                
+                difference += bwDiff;
+            }
             if(difference < lowestDifference)
             {
                 lowestDifference = difference;
--- Assets/Scripts/TextureChunkData.cs
+++ Assets/Scripts/TextureChunkData.cs
@@ -10,32 +10,4 @@ public class TextureChunkData //64x64
         tex.SetPixels32(colors);
         TextureUtility.WriteTexToPNG(tex, fileName);
     }
-
-    public int DiffWithChunk(TextureChunkData otherChunk)
-    {
-        int difference = 0;
-        for (int i = 0; i < colors.Length; i++)
-        {
-            difference += DiffBetweenColors(colors[i], otherChunk.colors[i]);
-        }
-
-        static int DiffBetweenColors(Color32 color1, Color32 color2)
-        {
-            //int rDiff = color1.r - color2.r;
-            //int gDiff = color1.g - color2.g;
-            //int bDiff = color1.b - color2.b;
-            //rDiff = rDiff < 0 ? -rDiff : rDiff;
-            //gDiff = gDiff < 0 ? -gDiff : gDiff;
-            //bDiff = bDiff < 0 ? -bDiff : bDiff;
-
-            double bw1 = color1.r * 0.3 + color1.g * 0.59 + color1.b * 0.11;
-            double bw2 = color2.r * 0.3 + color2.g * 0.59 + color2.b * 0.11;
-            double bwDiff = bw1 - bw2;
-            bwDiff = bwDiff < 0 ? -bwDiff : bwDiff;
-
-            return (int)bwDiff;
-            //return rDiff + gDiff + bDiff;
-        }
-        return difference;
-    }
 }
